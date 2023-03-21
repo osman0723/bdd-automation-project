4ab66b08cf9ddd24a1fb2100f98b35954e455367
osman0723 <osman23avcu@gmail.com>
2023-03-21 13:49:40 +0300

    updated

--- src/main/java/pageObjects/Base_PO.java
+++ src/main/java/pageObjects/Base_PO.java
@@ -5,11 +5,11 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-import utils.Global_Vars;
 
 import java.time.Duration;
 
@@ -34,36 +34,22 @@ public class Base_PO {
     }
 
     public void sendkeys(By by, String textToType){
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(Global_Vars.DEFAULT_EXPLICIT_TIMEOUT));
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
         wait.until(ExpectedConditions.elementToBeClickable(by)).sendKeys(textToType);
 
     }
     public void sendkeys(WebElement webElement, String textToType){
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(Global_Vars.DEFAULT_EXPLICIT_TIMEOUT));
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
         wait.until(ExpectedConditions.elementToBeClickable(webElement)).sendKeys(textToType);
 
     }
-    public void waitFor(By by){
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(Global_Vars.DEFAULT_EXPLICIT_TIMEOUT));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-    }
-    public void waitFor(WebElement element){
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(Global_Vars.DEFAULT_EXPLICIT_TIMEOUT));
-        wait.until(ExpectedConditions.visibilityOf(element));
-    }
     public void waitForWebElementAndClick(By by){
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(Global_Vars.DEFAULT_EXPLICIT_TIMEOUT));
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
         wait.until(ExpectedConditions.elementToBeClickable(by)).click();
     }
     public void waitForWebElementAndClick(WebElement element){
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(Global_Vars.DEFAULT_EXPLICIT_TIMEOUT));
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
         wait.until(ExpectedConditions.elementToBeClickable(element)).click();
-    }
-    public void waitForAlert_And_ValidateText(String text){
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(Global_Vars.DEFAULT_EXPLICIT_TIMEOUT));
-        wait.until(ExpectedConditions.alertIsPresent());
-        String alert_Message_Text = getDriver().switchTo().alert().getText();
-        Assert.assertEquals(alert_Message_Text,text);
     }
 
 }
--- src/main/java/pageObjects/Login_PO.java
+++ src/main/java/pageObjects/Login_PO.java
@@ -2,7 +2,6 @@ package pageObjects;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import utils.Global_Vars;
 
 public class Login_PO extends Base_PO{
     private @FindBy(id = "text")
@@ -15,7 +14,7 @@ public class Login_PO extends Base_PO{
         super();
     }
     public void navigateTo_WebDriverUniversity_LoginPage(){
-        navigateTO_URL(Global_Vars.WEBDRIVER_UNIVERSITY_HOMEPAGE_URL + "/Login-Portal/index.html?");
+        navigateTO_URL("https://www.webdriveruniversity.com/Login-Portal/index.html?");
     }
     public void setUsername(String username){
         sendkeys(usermane_TextField,username);
@@ -26,12 +25,4 @@ public class Login_PO extends Base_PO{
     public void clickOn_Login_Button(){
         waitForWebElementAndClick(login_button);
     }
-    public void validate_SuccessfulLogin_Message(){
-        waitForAlert_And_ValidateText("validation succeeded");
-    }
-    public void validate_UnsuccessfulLogin_Message(){
-        waitForAlert_And_ValidateText("validation failed");
-    }
-
-
 }
